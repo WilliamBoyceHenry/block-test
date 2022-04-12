52c3305325aad191ef4984d0fe64519fe485ec8a
WilliamBoyceHenry <103203282+WilliamBoyceHenry@users.noreply.github.com>
2022-04-12 08:06:27 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,12 +1,14 @@
 let T = 0
 let Mid = 0
+let Mid2 = 0
 let D = 0
 input.onButtonPressed(Button.A, function () {
     control.reset()
 })
 input.onButtonPressed(Button.B, function () {
     T = control.millis()
-    Mid = T / 10
-    D = Mid * 340
+    Mid = T / 1000
+    Mid2 = Mid * 340
+    D = Mid2 / 1609
     basic.showNumber(D)
 })
